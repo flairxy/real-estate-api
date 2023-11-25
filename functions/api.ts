51a9f8ba576e9f5da814d037a906344b783234c9
Poseidon <122105735+poseidonxy@users.noreply.github.com>
2023-11-25 02:53:30 +0100

    fix mongoose timeout error

--- functions/api.ts
+++ functions/api.ts
@@ -6,9 +6,6 @@ import serverless from 'serverless-http';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
 
-
-
-
 import { errorHandler } from '../src/middlewares/error-handler';
 import { NotFoundError } from '../src/errors/not-found-error';
 import { morganMiddleware } from '../src/middlewares/morgan';
@@ -42,39 +39,38 @@ app.all('*', async () => {
 });
 
 app.use(errorHandler);
-const start = async () => {
-    if (!process.env.CLOUDINARY_NAME || !process.env.CLOUDINARY_KEY || !process.env.CLOUDINARY_SECRET ) {
-      throw new Error('Cloudinary must be set');
-    }
-  
-    if (!process.env.SITE_URL) {
-      throw new Error('Site URL must be defined');
-    }
-  
-    if (!process.env.JWT_KEY) {
-      throw new Error('JWT_KEY must be defined');
-    }
-    if (!process.env.PAYSTACK_SECRET) {
-      throw new Error('Paystack payment must be set');
-    }
-    if (!process.env.MONGO_URI) {
-      throw new Error('MONGO_URI must be defined');
-    }
-    try {
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error(error);
-    }
-  };
+if (
+  !process.env.CLOUDINARY_NAME ||
+  !process.env.CLOUDINARY_KEY ||
+  !process.env.CLOUDINARY_SECRET
+) {
+  throw new Error('Cloudinary must be set');
+}
 
+if (!process.env.SITE_URL) {
+  throw new Error('Site URL must be defined');
+}
 
-  const handler = serverless(app);
-  module.exports.handler = async (event: any, context: any) => {
-    await start();
-    const result = await handler(event, context);
-    await mongoose.disconnect();
-    return result;
-  };
+if (!process.env.JWT_KEY) {
+  throw new Error('JWT_KEY must be defined');
+}
+if (!process.env.PAYSTACK_SECRET) {
+  throw new Error('Paystack payment must be set');
+}
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO_URI must be defined');
+}
 
+const dbExecute = (db: any, fn: any) => db.then(fn).finally(() => db.close());
 
+function dbConnectAndExecute(fn: any) {
+  return dbExecute(mongoose.connect(process.env.MONGO_URI!), fn);
+}
+
+const handler = serverless(app);
+module.exports.handler = async (event: any, context: any) => {
+  //close connection after request
+  dbConnectAndExecute(async () => {
+    return await handler(event, context);
+  });
+};
